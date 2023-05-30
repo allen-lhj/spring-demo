e14ef4452de5b2a88657a61912d4330311cf40ba
allen-lhj <liuhuanjun767@gmial.com>
2023-05-30 19:37:46 +0800

    init

---
+++ src/test/java/ltd/newbee/mall/NewbeeMallApplicationTests.java
@@ -0,0 +1,30 @@
+package ltd.newbee.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class NewbeeMallApplicationTests {
+	@Autowired
+	private DataSource defaultDataSource;
+
+	@Test
+	public void datasourceTest() throws SQLException {
+		// 获取数据库连接对象
+		Connection connection = defaultDataSource.getConnection();
+		System.out.print("获取连接：");
+		// 判断连接对象是否为空
+		System.out.println(connection != null);
+		connection.close();
+	}
+	@Test
+	void contextLoads() {
+
+	}
+
+}
